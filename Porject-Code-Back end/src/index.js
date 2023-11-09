c4cfa0568bab645269650f98a905a8415526f2ef
Jonathas Amaral <69598952+JonathasAmaral@users.noreply.github.com>
2023-11-08 22:41:12 -0300

    update database routes
    update database mongodb routes.
    npm install cors.

--- Porject-Code-Back end/src/index.js
+++ Porject-Code-Back end/src/index.js
@@ -2,22 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cardsRoutes = require('./routes/cards/card');
 const decksRoutes = require('./routes/decks/deck');
+const cors = require('cors');
 
 const app = express();
+
 const port = 3000;
 
 // site for create mongodb in cloud (https://www.mongodb.com/atlas/database)
 // Mudar link do banco de dados para o da sua base de dados do mongoDB
-mongoose.connect('mongodb+srv://<name>:<password>@cluster0.mqxhjth.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb://localhost:27017/flashCards')
     .then(() => {
       console.log('Conexão com o MongoDB estabelecida');
     })
     .catch((error) => {
       console.error('Erro ao conectar com o MongoDB:', error);
-    });// Connect to MongoDB database
+    }); // Connect to MongoDB database
 
-// for parsing application/json
+// for parsing application/json and cors
 app.use(express.json()); 
+app.use(cors());
 
 // Routes
 app.use('/cards', cardsRoutes);
